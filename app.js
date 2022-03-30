2a7c48f93230746ef9ad2d731f28925640e8961e
Vagan Pogosyan <vagan2502@gmail.com>
2022-03-30 18:18:22 -0400

    frequency counter bad code

--- app.js
+++ app.js
@@ -8,27 +8,47 @@
 //   console.log(arr[i]);
 //   i++;
 // }
-localStorage.clear();
 
-const mainDiv = document.createElement("div");
-mainDiv.className = "main-div";
-const h1 = document.createElement("h1");
-h1.innerText = "Hello world";
-mainDiv.append(h1);
-document.body.append(mainDiv);
-let html = "";
-let arr = [];
+// ==================================
 
-console.log(localStorage);
+// localStorage.clear();
 
-h1.onclick = (event) => {
-  html = `<div>${event.target.innerText}</div>`;
-  arr.push(html);
-  localStorage.setItem("items", arr);
+// const mainDiv = document.createElement("div");
+// mainDiv.className = "main-div";
+// const h1 = document.createElement("h1");
+// h1.innerText = "Hello world";
+// mainDiv.append(h1);
+// document.body.append(mainDiv);
+// let html = "";
+// let arr = [];
 
-  console.log(html);
-  console.log("array = " + arr);
-  // localStorage.setItem("itemsArray", html);
+// console.log(localStorage);
 
-  console.log(localStorage);
-};
+// h1.onclick = (event) => {
+//   html = `<div>${event.target.innerText}</div>`;
+//   arr.push(html);
+//   localStorage.setItem("items", arr);
+
+//   console.log(html);
+//   console.log("array = " + arr);
+//   // localStorage.setItem("itemsArray", html);
+
+//   console.log(localStorage);
+// };
+
+// ==========================
+
+function same(arr1, arr2) {
+  for (let el1 of arr1) {
+    for (let el2 of arr2) {
+      if (el2 === el1 ** 2) {
+        console.log(`${el1} = ${el2}`);
+      }
+    }
+  }
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 9, 1];
+
+same(arr1, arr2);
