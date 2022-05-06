ad9587cf2e3b15e4777b2eb0492066aa7c0ced14
Vagan Pogosyan <vagan2502@gmail.com>
2022-05-06 16:18:48 -0400

    finished mergeSort

--- app.js
+++ app.js
@@ -414,7 +414,49 @@ import { collectOdds, factorial, Recursion } from "./Recursion.js";
 // ==============================================================================
 // ============================== MERGE SORT ====================================
 // ==============================================================================
+// - Break up the array into halves until you have arrays that are empty or have one element in them
+// - Once you have smaller sorted arrays, merge those arrays with other sorted arrays until
+// you are back at the full length of the input array
+// - Once the arrays have been merged together return the merged (and sorted) array
 
-const arr = [3, 6, 1, 3, 9, 11];
+// helper function that merges two already sorted arrays
+// function merge(arr1, arr2) {
+//   let result = [];
+//   let i = 0;
+//   let j = 0;
+//   while (i < arr1.length && j < arr2.length) {
+//     if (arr2[j] > arr1[i]) {
+//       result.push(arr1[i]);
+//       i++; // while i < arr1.length
+//     } else {
+//       result.push(arr2[j]);
+//       j++;
+//     }
+//   }
+//   // while loops that keep pushing elements to the result array
+//   while (i < arr1.length) {
+//     result.push(arr1[i]);
+//     i++;
+//   }
+//   while (j < arr2.length) {
+//     result.push(arr2[j]);
+//     j++;
+//   }
+//   return result;
+// }
 
-function merge(arr1, arr2) {}
+// * both left and right portions of the input array keep breaking in halves recursively with
+// mergeSort function
+
+// function mergeSort(arr) {
+//   if (arr.length <= 1) return arr;
+//   let mid = Math.floor(arr.length / 2);
+
+//   let left = mergeSort(arr.slice(0, mid));
+//   let right = mergeSort(arr.slice(mid));
+
+//   return merge(left, right);
+// }
+
+// console.log(merge([1, 2, 293, 506], [5, 9, 12]));
+// console.log(mergeSort([6, 8, 2, 4, 3, 9, 12, 5, 23, 7, 1]));
